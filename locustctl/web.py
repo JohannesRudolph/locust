ced9b5fb14c1f04939d2cf2a1c2e68cc40adbe32
Johannes Rudolph <jojo.rudolph@gmail.com>
2016-07-11 12:17:42 +0200

    Fix locustctl api

--- locustctl/web.py
+++ locustctl/web.py
@@ -20,45 +20,55 @@ app.debug = True
 app.root_path = os.path.dirname(os.path.abspath(__file__))
 app.config['UPLOAD_FOLDER'] = 'uploads'
 
+if not os.path.exists('uploads'):
+    os.makedirs('uploads')
+
 locust_process = None
-locust_args = []
+locust_args = ""
 
 @app.route('/boot', methods=["POST"])
 def boot():
-    assert request.method == "POST"
+    global locust_process # we need write access to this variable
 
     if (locust_process != None):
-        return Response(json.dumps({message: "Locust already running"}, status=400, mimetype='application/json'))
+        return Response(json.dumps({'message': "Locust already running"}), status=400, mimetype='application/json')
 
     filename = 'locustfile.py'
     
     # check if the post request has the file part
     if (filename not in request.files):
-        return Response(json.dumps({message: "Request did not contain a locustfile.py"}, status=400, mimetype='application/json'))
+        return Response(json.dumps({'message': "Request did not contain a locustfile.py"}), status=400, mimetype='application/json')
 
-    host = request.form.host
+    host = request.form["host"]
     if (not host):
-        return Response(json.dumps({message: "Request did not contain a target host"}, status=400, mimetype='application/json'))
+        return Response(json.dumps({'message': "Request did not contain a target host"}), status=400, mimetype='application/json')
 
     file = request.files[filename]
     locustfile = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    
+    # ensure the path is clear
+    try:
+        os.remove(filename)
+    except OSError: 
+        pass
+
     file.save(locustfile)
     
     args = ["locust"]
-    args.extend(shlex.split(app.config["locust_args"]))
+    args.extend(shlex.split(locust_args))
     args.extend(["-f", locustfile])
     args.extend(["--host", host])
 
-    locust_process = subprocess.Popen(args, shell=true, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    locust_process = subprocess.Popen(args, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
-    return Response(json.dumps({message: "Locust started"}, status=400, mimetype='application/json'))
+    return Response(json.dumps({'message': "Locust started"}), status=200, mimetype='application/json')
 
 @app.route('/kill', methods=["POST"])
 def stop():
     if (locust_process != None):
         locust_process.kill()
 
-    return Response(json.dumps({message: "Locust has stopped"}, status=200, mimetype='application/json'))
+    return Response(json.dumps({'message': "Locust has stopped"}), status=200, mimetype='application/json')
 
 def start(options):
     locust_args = options.locust_args
